20a53e7314f77638e131b96e830d5ef9e9e51916
mikagitti <mikagitti@gmail.com>
2024-07-05 14:27:06 +0300

    Refactor: User logging button in navbar.

--- src/app/login/button.tsx
+++ src/app/login/button.tsx
@@ -1,25 +1,42 @@
-import { Box, Typography } from "@mui/material";
-import { ReactNode } from "react";
+import { Box, Typography } from '@mui/material';
+import { ReactNode } from 'react';
+import AssistWalkerIcon from '@mui/icons-material/AssistWalker';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
 
 type ComponentProps = {
-    children: ReactNode;
-    user: string | null;
-}
+  children: ReactNode;
+  user: string | null;
+  width?: string;
+  height?: string;
+};
 
-export default function LoginButton({children, user}: ComponentProps) {
+export default function LoginButton({ children, user, width, height }: ComponentProps) {
+  const boxStyle = {
+    width: width ? width : '100%',
+    height: height ? height : '100%',
+    display: 'flex',
+    alignItems: 'left',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    gap: '1',
+  };
 
-    return (    
-        <Box>
-            
-            <Typography sx={ {fontSize: '10px'} }>            
-                {children}                        
-            </Typography>
+  const textStyle = {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    width: '100%', // Ensure this is set so the ellipsis can work
+  };
 
-            <Typography sx={ {fontSize: '12px'} }>
-                {user}
-            </Typography>
+  return (
+    <Box sx={boxStyle}>
+      <Box>{user && <Typography sx={{ ...textStyle, fontSize: '12px' }}>{user}</Typography>}</Box>
 
-        </Box>    
-    );
-
+      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+        <AssistWalkerIcon />
+        <Typography sx={{ ...textStyle, fontSize: '10px' }}>{children}</Typography>
+        <EmojiPeopleIcon />
+      </Box>
+    </Box>
+  );
 }
