b46b47099b8c5a0d777f514ed69929d540faa215
mikagitti <mikagitti@gmail.com>
2024-12-13 15:39:19 +0200

    - Changed ManageShoppingList layout. - Added Add button and Remove button to ManageShoppingList.

--- src/app/shoppinglist_v2/[id]/manageshoppinglist/page.tsx
+++ src/app/shoppinglist_v2/[id]/manageshoppinglist/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { use, useContext, useEffect, useState } from "react";
 
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Container, styled, Typography, Theme } from "@mui/material";
+import Grid from '@mui/material/Grid2';
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
@@ -15,12 +16,26 @@ import {
 import {
      BoxStyle,
      ButtonStyle,
+     DangerButtonStyle,
      ListBoxStyle,
      ListItemStyle,
+     PrimaryButtonStyle,
 } from "../../components/Styles";
 import ProductListItem, { iconType } from "../../components/productListItem";
 import { ProductType, ShoppingListProductsType } from "@/Database/types";
 import LoginContext from "@/Context/login/LoginContext";
+
+const CustomStyledBox = styled(Box)(({ theme }) => ({
+     width: '15rem',
+     height: '4rem',
+     display: 'flex',
+     justifyContent: 'center',
+     alignItems: 'center',
+     color: 'white',
+     borderRadius: '10px',
+     boxShadow: theme.palette.mode === 'dark' ? '0 0 10px rgba(255, 255, 255, 0.3)' : '0 0 10px rgba(0, 0, 0, 0.3)',
+}));
+
 
 export default function Page({ params }: { params: Promise<{ id: number }> }) {
      const { id } = use(params);
@@ -38,16 +53,24 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
           color: "red",
      };
 
-     const [shoppingListProducts, setShoppingListProducts] = useState<
-          ShoppingListProductsType[]
-     >([]);
+     const [shoppingListProducts, setShoppingListProducts] = useState<ShoppingListProductsType[]>([]);
      const [allProducts, setAllProducts] = useState<ProductType[]>([]);
-     const [addingProductToShoppingList, setAddingProductToShoppingList] =
-          useState<boolean>(false);
+     const [addingProductToShoppingList, setAddingProductToShoppingList] = useState<boolean>(true);
 
      useEffect(() => {
           fetchShoppingListProductsToMemory();
+
+          const fetchProductToMemory = async () => {
+               setAllProducts(await GetAllProducts());
+          }
+
+          fetchProductToMemory();
+
      }, []);
+
+     useEffect(() => {
+          openAddProduct();
+     }, [addingProductToShoppingList]);
 
      useEffect(() => {
           fetchAwailableProductsToMemory();
@@ -63,10 +86,10 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
                );
                setAllProducts(filteredProducts);
           }
-          setAddingProductToShoppingList(!addingProductToShoppingList);
      };
 
      const fetchShoppingListProductsToMemory = async () => {
+          console.log("fetchShoppingListProductsToMemory");
           const shoppingListProducts: ShoppingListProductsType[] =
                await GetShoppingListProductsByShoppingListId(shoppingListId);
           setShoppingListProducts(shoppingListProducts);
@@ -77,11 +100,12 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
                (product) =>
                     !shoppingListProducts.some((b) => b.id === product.id)
           );
+          console.log("filteredProducts", filteredProducts);
           setAllProducts(filteredProducts);
      };
 
      //REMOVE
-     const removeProductFromList = async (productId: number) => {
+     const removeProductFromShoppingList = async (productId: number) => {
           await RemoveProductFromShoppingList(shoppingListId, productId);
           await fetchShoppingListProductsToMemory();
      };
@@ -106,37 +130,30 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
                     <Typography variant="h4">Manage shopping list.</Typography>
                </Box>
 
-               <Box sx={BoxStyle} m={1}>
-                    <Typography variant="body1" component="div">
-                         {addingProductToShoppingList
-                              ? "ADD products to shopping list"
-                              : "REMOVE products from shopping list"}
-                    </Typography>
-               </Box>
+               <Container>
+                    <Grid container spacing={2} m={2} justifyContent="center">
+                         <Button sx={PrimaryButtonStyle} onClick={() => setAddingProductToShoppingList(true)}>
+                              <Typography variant="h5" component="div">Add products</Typography>
+                         </Button>
+                         <Button sx={DangerButtonStyle} onClick={() => setAddingProductToShoppingList(false)}>
+                              <Typography variant="h5" component="div">Remove product</Typography>
+                         </Button>
+                    </Grid>
+               </Container>
 
                {shoppingListProducts.length <= 0 && (
                     <Box sx={BoxStyle} m={3}>
                          <Typography variant="h4" component="div">
-                              Shopping list is empty. Add products
+                              Shopping list is empty.<br />Add products
                          </Typography>
                     </Box>
                )}
 
                <Box sx={BoxStyle} m={1}>
-                    <Button sx={ButtonStyle} onClick={openAddProduct}>
-                         {addingProductToShoppingList ? (
-                              <Typography>Remove products from here</Typography>
-                         ) : (
-                              <Typography>Add products from here</Typography>
-                         )}
-                    </Button>
-               </Box>
-
-               <Box sx={BoxStyle} m={1}>
                     {addingProductToShoppingList ? (
                          <ProductListComponent products={allProducts} shownIcon={IconPropsForAddingProduct} action={addProductToShoppingList} />
                     ) : (
-                         <ProductListComponent products={shoppingListProducts} shownIcon={IconPropsForDeletingProduct} action={removeProductFromList} />
+                         <ProductListComponent products={shoppingListProducts} shownIcon={IconPropsForDeletingProduct} action={removeProductFromShoppingList} />
                     )}
                </Box>
 
